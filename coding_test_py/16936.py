65e1d9422c6118b2533b4d7604fb49ccf4885017
heonoo <zkfktm1525@naver.com>
2022-08-08 14:21:25 +0900

    modify 16936.py

--- coding_test_py/16936.py
+++ coding_test_py/16936.py
@@ -7,14 +7,15 @@ N = int(input())
 nums = list(map(int, input().split()))
 nums_cs = Counter(nums)
 
+'''brute-force
 def dfs(depth):
     if depth == N:
         return True
     else:
-        for num in nums:
+        for num in ans[-1] // 3, ans[-1] * 2:
             if nums_cs[num] and \
-               ((ans[-1] // 3 == num and ans[-1] % 3 == 0) or \
-               ans[-1] * 2 == num):
+                ((ans[-1] // 3 == num and ans[-1] % 3 == 0) or \
+                ans[-1] * 2 == num):
                 ans.append(num)
                 nums_cs[num] -= 1
                 if dfs(depth+1):
@@ -25,6 +26,27 @@ def dfs(depth):
 
 for num in nums:
     ans = [num]
+    nums_cs[num] -= 1
     if dfs(1):
         break
+    nums_cs[num] += 1
+'''
+# topology sort
+
+for num in nums:
+    if nums_cs[num*2]:
+        continue
+    if num % 3 == 0 and nums_cs[num//3]:
+        continue
+    end_num = num
+    break
+
+ans = [end_num]
+while len(ans) < N:
+    if ans[-1] % 2 == 0 and nums_cs[ans[-1]//2]:
+        ans.append(ans[-1]//2)
+    else:
+        ans.append(ans[-1]*3)
+
+ans.reverse()
 print(' '.join(map(str, ans)))
